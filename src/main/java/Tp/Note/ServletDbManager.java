201c42a531a4000970d9fe61a6a5cf6858bd75e1
Quentin <quentin.ceschin@gmail.com>
2020-04-01 19:16:10 +0200

    add function to create a db con

--- src/main/java/Tp/Note/ServletDbManager.java
+++ src/main/java/Tp/Note/ServletDbManager.java
@@ -7,11 +7,21 @@ package Tp.Note;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  *
@@ -84,5 +94,95 @@ public class ServletDbManager extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+ public void createTableUser(Connection con) throws SQLException{
+         Statement ps = con.createStatement();
+        try {
+            String query = "CREATE TABLE USER( ";
+            query += "id INT PRIMARY KEY NOT NULL,";
+            query += "nom VARCHAR(100),";
+            query += "prenom VARCHAR(100),";
+            query += "email VARCHAR(255),";         
+            query += "privilege INT)";
 
+
+            ps.executeUpdate(query);    
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+    }
+    public void dropTableUser(Connection con) throws SQLException{
+         Statement ps = con.createStatement();
+        try {
+            ps.executeUpdate("DROP TABLE USER");    
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+    }
+    public void resetTableUser(Connection con) throws SQLException{
+        dropTableUser(con);
+        createTableUser(con);
+        generateUser(con);
+        
+    }
+    
+    public ResultSet selectListUser(Connection con) throws SQLException{
+        Statement ps = con.createStatement();
+        String query = "SELECT nom,prenom,email,privilege from USER";
+        ResultSet rs = null;
+        try {
+         rs = ps.executeQuery(query);    
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        finally{
+            return rs;
+        }
+    }
+    
+    public void generateUser(Connection con) throws SQLException{
+         Statement ps = con.createStatement();
+        String query = "INSERT INTO USER (nom,prenom,email,privilege) values ";
+        query += "('Bonbeure','Jean','jean.bonbeure@test.mail,1'),";
+        query += "('Colique','Al','al.colique@test.mail,1'),";
+        query += "('Jean','Damien','damien.jean@test.mail,1')";
+        try {
+         ps.executeUpdate(query);    
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+    }
+    
+    public void deleteUser(Connection con, String name) throws SQLException{
+         Statement ps = con.createStatement();
+         String query = "DELETE from USER where nom='"+name+"'";
+        try {
+         ps.executeUpdate(query);    
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+    }
+    public Connection createConnexion(){
+        Context initCtx=null;
+        try {
+            initCtx = new InitialContext();
+        } catch (NamingException ex) {
+            System.out.println("Erreur de chargement du service de nommage");
+        } 
+
+        // Connexion ? la base de donn?es enregistr?e dans le serveur de nom sous le nom "sample"
+        Object refRecherchee = null;
+        try {
+            refRecherchee = initCtx.lookup("jdbc/__default");
+        } catch (NamingException ex) {
+            Logger.getLogger(ServletDbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        DataSource ds = (DataSource)refRecherchee;
+        Connection con = null;
+        try {
+            con = ds.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(ServletDbManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
 }
